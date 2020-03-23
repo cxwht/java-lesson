7cea9915a4f0477c0d54510aa92f737ac1dec1da
cxwht <txb018@163.com>
2020-03-23 16:06:10 +0800

    第三次作业更新

--- Student/src/ArrayTest.java
+++ Student/src/ArrayTest.java
@@ -1,37 +1,39 @@
 public class ArrayTest {
     public static void main(String[] args) {
-        String s = "sadeweweaaa34324sdsd3432sdf";
+        work1("sadeweweaaa34324sdsd3432sdf");
+        System.out.println("");
+        System.out.println("==================");
+        System.out.println("");
+        work2("e22fffs33tttdd");
+    }
+    public static void work1(String s) {
         int count = 0;
         int countNum = 0;
 
         for (int i = 0; i < s.length(); i++) {
-
             if (s.charAt(i) == 'a') {
                 count++;
             }
             if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
                 countNum++;
             }
-
         }
-
         System.out.println("字符串s中字母a出现的次数有" + count + "次");
-
         System.out.println("字符串s中数字出现的次数有" + countNum + "次");
-        System.out.println("=======================");
-        String s2 = "e22fffs33tttdd";
+    }
+    public static void work2(String s) {
         int flag;
         char[] result = new char[20];
-        for (int i = 0, k = 0; i < s2.length(); i++) {
+        for (int i = 0, k = 0; i < s.length(); i++) {
             flag = 0;
-            for (int j = i + 1; j < s2.length(); j++) {
-                if (s2.charAt(j) == s2.charAt(i)) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(j) == s.charAt(i)) {
                     flag = 1;
                     break;
                 }
             }
             if (flag == 0) {
-                result[k] = s2.charAt(i);
+                result[k] = s.charAt(i);
                 k++;
             }
         }
