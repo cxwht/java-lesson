52a8aac6bc8d7b2be31af7af423b583a60e545a3
cxwht <txb018@163.com>
2020-03-26 19:46:05 +0800

    改进

--- Student/src/calc.java
+++ Student/src/calc.java
@@ -23,6 +23,9 @@ public class calc {
         op = sc.nextInt();
         return op;
     }
+    public static double round(double value){
+        return Math.round(value*100)/100.0;
+    }
     public static void foo(int op) {
         double x,y,res = 1.2;
         switch(op) {
@@ -31,30 +34,35 @@ public class calc {
                 x = sc.nextDouble();
                 System.out.println("输入第二个加法数");
                 y = sc.nextDouble();
+                x = isfloat(x);
+                y = isfloat(y);
                 res = x + y;
-                init();
                 break;
             case 2:
                 System.out.println("请输入第一个减法数");
                 x = sc.nextDouble();
                 System.out.println("输入第二个减法数");
                 y = sc.nextDouble();
+                x = isfloat(x);
+                y = isfloat(y);
                 res = x - y;
-                init();
                 break;
             case 3:
                 System.out.println("请输入第一个乘法数");
                 x = sc.nextDouble();
                 System.out.println("输入第二个乘法数");
                 y = sc.nextDouble();
+                x = isfloat(x);
+                y = isfloat(y);
                 res = x * y;
-                init();
                 break;
             case 4:
                 System.out.println("请输入第一个除法数");
                 x = sc.nextDouble();
                 System.out.println("输入第二个除法数");
                 y = sc.nextDouble();
+                x = isfloat(x);
+                y = isfloat(y);
                 if (x % 0.1 == 0) {
                     x = x;
 
@@ -67,12 +75,12 @@ public class calc {
                     System.out.println("除数不能为0！请重新开始");
                 }
                 res = x / y;
-                init();
                 break;
             default:
                 break;
         }
         System.out.println("结果为"+res);
+        init();
     }
     public static void get(int op) {
         switch(op) {
@@ -95,7 +103,23 @@ public class calc {
             default:
                 System.out.println("请输入正确的字符!!");
         }
-
-
+    }
+    public static double isfloat(double x) {
+        String s = Double.toString(x);
+        int pos = s.indexOf('.');
+        if(pos != -1){
+            if(s.length() > pos + 2) {
+                x = Double.parseDouble(s)*10 %10;
+                x = round(x);
+                System.out.println(x);
+            }else {
+                x = Double.parseDouble(s);
+                System.out.println(x);
+            }
+        } else {
+            x = Double.parseDouble(s);
+            System.out.println(x);
+        }
+        return (double)x;
     }
 }
